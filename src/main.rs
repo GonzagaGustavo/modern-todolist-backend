459375972c87bf3b392ff0742dd2c56e7bb4a03a
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2023-05-19 21:51:21 -0300

    tentando adicionar algo no banco de dados

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use mysql::prelude::*;
 use mysql::*;
+use serde_json::Value;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -25,16 +26,43 @@ struct List {
 
 #[get("/")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
+    HttpResponse::Ok().body("Modern ToDoList")
 }
 
 #[post("/add")]
-async fn add() -> impl Responder {
-    let url = "mysql://root:root@localhost:8889/modern_todolist";
-    let conn = Conn::new(url)?;
+async fn add(req_body: web::Bytes) -> Result<HttpResponse, std::io::Error> {
+    let url: &str = "mysql://root:root@localhost:8889/modern_todolist";
+    let conn = Conn::new(url).unwrap();
+    let body = String::from_utf8(req_body.to_vec()).unwrap();
+    let json_body: Value = serde_json::from_str(&body)?;
+    let content = json_body["content"]
+        .as_str()
+        .ok_or_else(|| actix_web::error::ErrorBadRequest("Missing field 'content'"))
+        .unwrap();
+    conn.exec_drop(r"INSERT INTO list (content) VALUES (?)", &content)
+        .unwrap();
 
-    HttpResponse::Ok().body("success")
+    Ok(HttpResponse::Ok().body("success"))
 }
+
+// #[get("/list")]
+// async fn list() -> std::result::Result<impl Responder, Box<dyn std::error::Error>> {
+//     let conn = get_conn();
+
+//     let list = conn.query_map("SELECT * FROM list", |(id, content, created)| List {
+//         id,
+//         content,
+//         created,
+//     })?;
+
+//     HttpResponse::Ok().body(list)
+// }
+
+fn get_conn() -> std::result::Result<Conn, Box<dyn std::error::Error>> {
+    let url = "mysql://root:root@localhost:8889/modern_todolist";
+    return Ok(Conn::new(url)?);
+}
+
 async fn hey() -> impl Responder {
     HttpResponse::Ok().body("Hey!")
 }
